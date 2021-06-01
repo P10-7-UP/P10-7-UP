fdd5c258a32861f552f093b8f125c2473e6e39cb
P10-7-UP <7upspacefight@gmail.com>
2021-06-01 21:56:27 +0700

    commit innit

---
+++ master_code_7-UP/master_code_7-UP.ino
@@ -0,0 +1,134 @@
+//Master Code : Team 7-UP
+#include <Wire.h> //I2C
+
+#define PAY_ADR    0x14 // Payload I2C Address
+//Register Map
+#define PAY_ID     0x14 // PAYLOAD Address
+#define PAY_SS     0x02 // PAYLOAD Status Address
+#define PAY_DATA_L 0xB0 // PAYLOAD Low Data & Data Address
+#define PAY_DATA_H 0xB1 // PAYLOAD High Data
+#define PAY_PM     0xAA // PAYLOAD Power Management(Reset)
+
+#define CM_PAY_ID  0x31 //Register for command ID&Address
+#define CM_PAY_SS  0x32 //Register for command Status
+
+void setup()
+{
+  //initial temp. variable for payload data
+  byte id;
+  byte payload_status;
+
+  Wire.begin(); //คำสั่งให้เป็นตัว Master
+
+  Serial.begin(9600); // start serial for debug at baudrate 9600
+ 
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  //Initial Message Guideline For Command Payload 
+  Serial.println("#####+++++ Payload Function Command +++++#####");
+  Serial.println("______________________________________________\n"); 
+  Serial.println("Enter '1' : Get Payload ID & Payload Address");
+  Serial.println("Enter '2' : Get Data From Payload");
+  Serial.println("Enter '3' : View Payload Status");
+  Serial.println("Enter '9' : Reset Power Management on Payload "); 
+  Serial.println("______________________________________________\n");
+  
+  request_byte(PAY_ADR, PAY_ID, &id, 1);             //request id
+  request_byte(PAY_ADR, PAY_SS, &payload_status, 1); //request status
+
+  Serial.println("Payload ID : " + String(id)); //Show Payload ID 
+  Serial.println("Payload Status : 0B" + String(payload_status, BIN) + "\n"); //Show Payload Status Address 
+}
+
+void loop()
+{
+  byte payload_data[2]; // data[0] -> low , [1] -> high
+  byte payload_status;  //Payload Status Value
+  byte payload_adr;     //Payload Address Value
+  byte payload_pm;      //Payload Reset Value
+
+  if (Serial.available())
+  {
+
+    int c = -99;
+    while (Serial.available())
+    {
+      c = Serial.parseInt();
+      Serial.read();
+    }
+    Serial.println(c);
+    if (c == 1) //command slave to Get Payload ID & Address
+    {
+      byte data_send = 0b00010100;              //create buffer to command Payload ID&Address
+      write_byte(PAY_ADR, CM_PAY_ID, data_send); //write buffer to #PAY_ADR ,at register : #CM_PAY_ID,buffer #data_send
+      Serial.println("Command Get Payload ID & Address!"); 
+     
+      request_byte(PAY_ADR, PAY_ID, &payload_adr, 1 ); //request #PAY_ADR, to send Payload Address back, save it on #payload_adr,1 byte
+      Serial.println("Payload ID : " + String(payload_adr,DEC));        //Show ID
+      Serial.println("Payload Address : 0x" + String(payload_adr,HEX) + "\n"); //Show Address
+    }
+    else if (c == 2) //command slave to Get Data From Payload
+    {
+      Serial.println("Command Get Data From Payload!"); 
+ 
+      request_byte(PAY_ADR, PAY_DATA_L, payload_data , 2 ); //request #PAY_ADR, to send Payload Data back, save it on #payload_data,2 byte
+        //decode data from 2 byte [0-> low , 1 -> high]
+        //payload_data[0] = 0bllllllll
+        //payload_data[1] = 0bhhhhhhhh
+        //paydata = 0bhhhhhhhhhllllllll and convert to int, after below calculation 
+      int pay_data = payload_data[0] + (payload_data[1] << 8);
+        //Show Result From Payload
+      //Serial.println("Payload UV Data : " + String(pay_data,DEC) + " mV. ,From Bit:(Low:" + String(payload_data[0], DEC) + "),(High:" 
+     // + String(payload_data[1], DEC)+ ") \n");
+      Serial.println("Payload UV Data : " + String(pay_data,DEC) + " mV.");
+    }
+    else if (c == 3) //command slave to Get Payload Status
+    {
+      byte data_send = 0b00001100;               //create buffer to command Payload Status
+      write_byte(PAY_ADR, CM_PAY_SS, data_send); //write buffer to #PAY_ADR ,at register : #CM_PAY_SS,buffer #data_send
+      Serial.println("Command Get Payload Status!");
+      
+      request_byte(PAY_ADR, PAY_SS, &payload_status, 1 ); //request #PAY_ADR, to send Payload Status back, save it on #payload_status,1 byte
+      Serial.println("Payload Status: 0x" + String(payload_status, HEX) + " == 0B" + String(payload_status, BIN) + "\n"); //Show Status HEX , Binary code
+    }
+    else if (c == 9)//command slave to Reset Payload
+    {
+      byte data_send = 0b00000001;            //create buffer to command Payload Power Management 
+      write_byte(PAY_ADR, PAY_PM, data_send); //write buffer to #PAY_ADR ,at register : #PAY_PM,buffer #data_send
+      Serial.println("Command Power Management (Reset Payload)!");
+      
+      request_byte(PAY_ADR, PAY_PM, &payload_pm, 1);  //request #PAY_ADR, to send Command Reset Payload back, save it on #payload_pm,1 byte
+      delay(1000); //waiting Payload Reset
+      Serial.println("Payload Reset Successfully \n"); //Show Payload Reset finished
+    }
+  }
+}
+
+void write_byte(const int slaveAddress, const int registerAddress, byte data_send)
+{
+  Wire.beginTransmission(slaveAddress); //คำสั่งให้ Master ส่งข้อมูลให้ Slave
+  Wire.write(registerAddress);          //ให้ส่งข้อมูล register ไปให้ตัว Slave
+  Wire.write(data_send);
+  Wire.endTransmission(); //หยุดการส่ง
+  delay(1); //wait to already send 
+  //up to i2c data transfer rate if 100kHz -> use approx. time : 1 mSecond   
+}
+
+void request_byte(const int slaveAddress, const int registerAddress, byte *data_in, const int nBytes)
+{
+  Wire.beginTransmission(slaveAddress);   //คำสั่งให้ Master ส่งข้อมูลให้ Slave
+  Wire.write(registerAddress);            //ส่งข้อมูล register ที่ต้องการ ไปให้ตัว Slave
+  Wire.endTransmission();                 //หยุดการส่ง
+  delay(250);                               //delay 250 millisecond for slave prepare data
+  Wire.requestFrom(slaveAddress, nBytes); //ขอข้อมูลจาก Slave
+  int i = 0;
+  while (Wire.available())
+  {
+    data_in[i++] = Wire.read();
+  }
+  if (i != nBytes)
+    Serial.println("Error!!! :" + String(i) + String(nBytes));
+}
---
+++ slave_receiver_7-UP/slave_receiver_7-UP.ino
@@ -0,0 +1,194 @@
+//Payload Code : Team 7-UP
+#include <Wire.h> //I2C
+
+//Slave Register Address Map
+#define PAY_ID      0x14 // PAYLOAD Address
+#define PAY_SS      0x02 // PAYLOAD Status Address
+#define PAY_DATA_L  0xB0 // PAYLOAD Low Data & Data Address
+#define PAY_DATA_H  0xB1 // PAYLOAD High Data
+#define PAY_PM      0xAA // PAYLOAD Power Management(Reset)
+
+#define CM_PAY_ID   0x31 //Register for command ID&Address
+#define CM_PAY_SS   0x32 //Register for command Status
+
+//Slave Register value
+#define PAY_ID_VAL  0x14 // Payload ID
+#define SLAVE_ADRR  0x14 // Address for I2C Bus Communication
+
+//initial data buffer from master
+//at array[0] -> received payload register address
+//at array[1] -> data to write data to payload register
+byte registerRequested[2] = {0xFF,0xFF};
+
+//Define flag Command Payload
+bool isReceived = 0;
+bool isRequestData =0;
+bool cmd_id =0;
+bool cmd_data = 0;
+bool cmd_ss = 0;
+bool cmd_pm = 0;
+
+int voltageA0 = 0; //variable check voltage
+
+int uv_data=0; //variable sensor data
+
+byte pay_ss_val;
+byte return_byte = 0xFF;
+byte return_array[2];
+byte pay_val_data_l;
+byte pay_val_data_h;
+byte pay_pm_value = 0b00000000;
+
+void setup(){
+
+  Wire.begin(SLAVE_ADRR);                // join i2c bus with address #8
+  
+  pinMode(LED_BUILTIN,OUTPUT);
+
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);           // start serial for output
+  Serial.println("Slave Address: 0x" + String(SLAVE_ADRR, HEX) + " initial success!! \n");
+
+}
+
+void(* resetFunc) (void) = 0; //Function Reset
+
+void  Reset_Payload() //Function Reset Payload
+{
+    Serial.println("Master Command --> Reset Payload! \n");
+    delay(1000);
+    resetFunc();
+}
+
+int Get_Sensor(){ //Function UV Sensor
+  long sum=0;
+for(int i=0;i<1024;i++){
+  int sensorValue = analogRead(A0);
+  sum=sensorValue+sum;
+}
+  sum =sum >>10;
+  uv_data = sum*4980/1023; 
+}
+
+void loop() { //Loop Running
+
+  //Read Sensor Status from analog
+  voltageA0 = analogRead(A0);
+
+  bool reader1_ss = 1; //Logic for Read Voltage Pin analog : A0
+
+  //if (voltageA0 == 0)
+  if (voltageA0 != 0) //update code
+  {
+    //reader1_ss = 1;  //Logic = 0 if Voltage == 0
+    reader1_ss = 1;  //Logic = 1 if Voltage != 0 update code
+  }
+
+  pay_ss_val =  1 * 8 + reader1_ss * 4 + reader1_ss * 2 + reader1_ss - reader1_ss * 3 ;
+  //pay_ss_val = 1<<3|reader1_ss<<2|reader1_ss<<1|reader1_ss<<0 - reader1_ss * 3;
+
+  if (isReceived) //if Master -->> Request Data From Payload
+  {
+    //clear flag = 0
+    isReceived=0;
+    
+    switch (registerRequested[0]) //check data 
+    {
+      case PAY_ID:      return_byte = PAY_ID_VAL;   break; //Return Payload ID & Address --> Master
+
+      case CM_PAY_ID:   cmd_id = 1;                 break; //Master --> Request Payload ID & Address
+                        
+      case PAY_SS:      return_byte = pay_ss_val;   break; //Return Payload Status --> Master
+      
+      case CM_PAY_SS:   cmd_ss=1;                   break; //Master --> Request Payload Status
+                        
+      case PAY_DATA_L:  isRequestData = 1;          break; //Master --> Request Payload Data
+      case PAY_PM:      cmd_pm = 1;                 break; //Master --> Command Reset Payload
+      default :         return_byte = 0xFF;         break;
+    }
+
+    if(isRequestData) //if Master -->> Request Sensor Data From Payload
+    {
+      Get_Sensor(); //Start UV Sensor Function
+
+      pay_val_data_l = uv_data & 0x000000FF ; //uv_data % 255 
+      pay_val_data_h = uv_data  >> 8 ;        //if( uv_data > 255 ) >> 8
+
+      //set return byte array
+      return_array[0] = pay_val_data_l; 
+      return_array[1] = pay_val_data_h;
+
+      //debug message Show Register , Sensor Data , Bit(Low,High)
+      Serial.print("Register:0x" + String(registerRequested[0], HEX) + " , ");
+      Serial.println("Sending UV Data : " + String(uv_data, DEC) + " mV." + "\n");
+      /*,(Low:");
+      Serial.print(return_array[0]);
+      Serial.print(" At Register:0x" + String(PAY_DATA_L, HEX) + " , High:");
+      Serial.print(return_array[1]);
+      Serial.println(" At Register:0x" + String(PAY_DATA_H, HEX) + " ) -->Master \n");*/
+      delay(20);
+    }
+    
+    if (cmd_id == 1) //Command ID & Address
+    {
+      cmd_id =0; //clear flag = 0
+      Serial.print("Register: 0x" + String(registerRequested[0], HEX)); //Show Register the Master Send to Payload
+      Serial.println(" , Data send : " + String(registerRequested[1], HEX) + " -->From Master"); //Show data_send from Master
+      Serial.println("Sending Payload ID : " + String(PAY_ID_VAL, DEC) + " -->Master");  //Show Payload ID 
+      Serial.println("Sending Payload Address : 0x" + String(PAY_ID_VAL, HEX) + " -->Master \n"); //Show Payload Address
+    }
+    if (cmd_ss == 1) //Command Payload Status 
+    {
+      cmd_ss =0; //clear flag = 0
+      Serial.print("Register: 0x" + String(registerRequested[0], HEX)); //Show Register the Master Send to Payload
+      Serial.println(" , Data send : " + String(registerRequested[1], HEX) + " -->From Master"); //Show data_send from Master
+      Serial.println("Register Send Status : 0B" + String(pay_ss_val, BIN) + " -->Master \n"); //Show Payload Status
+    }
+    if (cmd_pm == 1) //Command Reset Payload
+    {
+      cmd_pm =0; //clear flag = 0
+      Serial.print("Register: 0x" + String(registerRequested[0], HEX)); //Show Register the Master Send to Payload
+      Serial.println(" , Data send : " + String(registerRequested[1], HEX) + " -->From Master"); //Show data_send from Master
+      pay_pm_value = registerRequested[1]; //set register from master
+      Reset_Payload(); //Start Reset Payload Function
+    }
+  }
+  //delay(25); //for reduce loop iteration time
+}
+
+void requestEvent() //check isRequest data and send {buffer data} back to master
+{
+  if(isRequestData) //check process is request sensor data 
+  {
+    isRequestData=0;
+    
+    //send return byte array [ 1byte / 1write() ] 
+    Wire.write(return_array[0]);
+    Wire.write(return_array[1]);
+    
+    //clear flag
+    return_array[0] = 0xFF;
+    return_array[1] = 0xFF;
+  }
+  //send return data to master
+  Wire.write(return_byte);
+  //clear buffer
+  return_byte = 0xFF;
+}
+
+void receiveEvent(int bytes)
+{
+  //set received flag to 1 and read from master
+  isReceived = 1;//รับข้อมูลตัวอักษรตามจำนวน byte แล้วพิมพ์ออกมา
+  int i =0; //set intial received index array 
+  while (Wire.available())
+  {
+    // read i2c data from master-> buffer #registerRequested
+    // set buffer at i and i = i+1 to change received index
+    registerRequested[i++] = Wire.read(); 
+  }
+  if (i != bytes) //total received not equal to bytes on bus
+    //decode message occur Error
+    Serial.println("Error!!! :" + String(i) + String(bytes));
+}
